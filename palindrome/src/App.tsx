38ea75d7af88a350811c5a74d37aeff3c4e1b101
Rishabkhivsara <rishabjain3500@gmail.com>
2022-09-24 04:02:29 +0530

    Add: new method to check palindrome

--- palindrome/src/App.tsx
+++ palindrome/src/App.tsx
@@ -13,7 +13,7 @@ const reversedString = (str: string) => {
 
 const isPalindrome = (str: string): boolean => {
   const reversed = reversedString(str);
-  console.log("reversed", reversed);
+  console.log(`{original,reversed} :`, str, reversed);
 
   return str === reversed;
 };
@@ -21,7 +21,7 @@ const isPalindrome = (str: string): boolean => {
 const getAllDateFormats = (date: string) => {
   const dateInStr = date.split("-");
 
-  console.log(dateInStr);
+  console.log(dateInStr); // array type
 
   const ddmmyyyy = dateInStr.join("");
   const mmddyyyy = dateInStr[1] + dateInStr[2] + dateInStr[0];
@@ -35,17 +35,92 @@ const getAllDateFormats = (date: string) => {
 
 const checkPalindromeForAllDateFormats = (date: string) => {
   const listOfPalindromes = getAllDateFormats(date);
+  console.log(listOfPalindromes);
 
   let flag = false;
 
   listOfPalindromes.map((item) => {
-    console.log("original", item);
     if (isPalindrome(item)) {
       return (flag = true);
     }
   });
 
   return flag;
+};
+
+const checkLeapYear = (year: number) => {
+  if (year % 400 === 0 || year % 100 !== 0 || year % 4 === 0) {
+    return true;
+  }
+  return false;
+};
+
+// const getNextDate = (date: string) => {
+//   let counter = 0;
+//   let currentDate = new Date(date);
+//   let nextDate = currentDate.setDate(currentDate.getDate() + 1);
+//   let day = currentDate.getDate() + 1;
+//   let month = currentDate.getMonth();
+//   let year = currentDate.getFullYear();
+
+//   let daysInMonth = [31, 28, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31];
+
+//   if (month === 2) {
+//     if (checkLeapYear(year)) {
+//       if (day > 29) {
+//         day = 1;
+//         month = 3;
+//       }
+//     } else {
+//       if (day > 28) {
+//         day = 1;
+//         month = 3;
+//       }
+//     }
+//   } else {
+//     if (day > daysInMonth[month - 1]) {
+//       day = 1;
+//       month++;
+//     }
+//   }
+
+//   if (month > 12) {
+//     month = 1;
+//     year++;
+//   }
+// };
+
+const _getNextDate = (date: string) => {
+  let currentDate = new Date(date);
+  let nextDate = new Date(currentDate.setDate(currentDate.getDate() + 1));
+  // let nextDate2 = new Date(currentDate.getTime() + 24 * 60 * 60 * 1000);
+  let day = currentDate.getDate() + 1;
+  let month = nextDate.getMonth();
+  let year = nextDate.getFullYear();
+
+  let obj = {
+    currentDate,
+    nextDate,
+    // nextDate2,
+    day,
+    month,
+    year,
+  };
+
+  console.table(obj);
+  return `${year}-${month}-${day}`;
+};
+
+const checkNextPalindromeDate = (date: string) => {
+  let counter = 0;
+  let flag = true;
+
+  while (flag) {
+    flag = !checkPalindromeForAllDateFormats(_getNextDate(date));
+    counter++;
+  }
+  console.log(counter);
+  return counter;
 };
 
 const App = () => {
@@ -63,6 +138,7 @@ const App = () => {
     if (checkPalindromeForAllDateFormats(date)) {
       setOutput("Astonishing, you were born on a Palindrome day");
     } else {
+      // checkNextPalindromeDate(date);
       setOutput("Bad luck");
     }
   };
